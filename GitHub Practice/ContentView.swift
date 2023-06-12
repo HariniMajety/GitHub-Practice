e402b511603b621205e2b67a1631612ec3932f06
Harini Majety <harinipmajety@gmail.com>
2023-06-12 10:18:18 -0500

    Initial Commit

---
+++ GitHub Practice/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitHub Practice
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Practice commiting/sending and sharing")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
